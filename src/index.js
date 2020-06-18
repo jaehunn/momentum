2652d111ba0a2b2ec85cd8a4917fe08bddd76c8b
bangjaehun <qkdwogns98@gmail.com>
2020-06-18 19:10:24 +0900

    ui

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const r = React;
+const rd = ReactDOM;
+const e = r.createElement;
+
+document.addEventListener("DOMContentLoaded", () => {
+  rd.render(e(App), document.getElementById("root"));
+});
+
+const App = () => {
+  // state
+
+  return e("div", { className: "app" }, e(Header), e("div", { className: "container" }, e(List), e(Note)));
+};
+
+const Header = () => {
+  return e(
+    "header",
+    {},
+    e("div", { className: "title" }, e("span", {}, "Simple Note")),
+    e("div", { className: "btns-group" }, e("button", {}, "ADD"), e("button", {}, "DELETE"))
+  );
+};
+
+const List = () => {
+  return e("div", { className: "list" }, e(ListItem), e(ListItem));
+};
+
+const ListItem = () => {
+  return e(
+    "div",
+    { className: "list-item" },
+    e("div", { className: "title" }, "Title"),
+    e("div", { className: "list-contents" }, "Contents")
+  );
+};
+
+const Note = () => {
+  return e(
+    "div",
+    { className: "note" },
+    e("input", { className: "title" }),
+    e("textarea", { className: "note-contents" })
+  );
+};
