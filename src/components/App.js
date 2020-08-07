c2b1d133b35f283c08fffd24bbe5ba77d1757433
bangjaehun <qkdwogns98@gmail.com>
2020-08-07 14:47:21 +0900

    Name...

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,37 @@ import React from 'react';
 import Name from './Name';
 
 class App extends React.Component {
+  state = {
+    name: '',
+  };
+
+  setName = (text) => {
+    this.setState({
+      name: text,
+    });
+
+    localStorage.setItem('NAME', text);
+  };
+
+  getName = () => {
+    const name = localStorage.getItem('NAME');
+
+    if (name !== null)
+      this.setState({
+        name,
+      });
+  };
+
+  componentDidMount() {
+    this.getName();
+  }
+
   render() {
+    const { name } = this.state;
+
     return (
       <div className="App">
-        <Name />
+        {name !== null ? name : <Name saveName={this.saveName} />}
       </div>
     );
   }
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Name.css';
 
 class Name extends React.Component {
-  state = {};
+  state = {
+    text: '', // local
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    const { name, value } = e.target;
+
+    e.preventDefault();
+
+    this.setState({
+      [name]: '', // reset
+    });
+  };
 
   render() {
-    return <div className="Name">Name</div>;
+    return (
+      <div className="Name">
+        <form onSubmit={this.handleChange}>
+          <div></div>
+        </form>
+      </div>
+    );
   }
 }
 
