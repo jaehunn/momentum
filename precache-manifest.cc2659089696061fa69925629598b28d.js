89a0e0c7dac438978f6cc61d9bfbd9335f34c50b
bangjaehun <qkdwogns98@gmail.com>
2020-08-08 12:33:30 +0900

    Updates

---
+++ precache-manifest.cc2659089696061fa69925629598b28d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0af6375b267a2ffc5383169f3b6f2e",
+    "url": "/momentum/index.html"
+  },
+  {
+    "revision": "251975d0880115738d5a",
+    "url": "/momentum/static/css/main.052983bf.chunk.css"
+  },
+  {
+    "revision": "2e0b1c3c343fb57a5017",
+    "url": "/momentum/static/js/2.5deec308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/momentum/static/js/2.5deec308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251975d0880115738d5a",
+    "url": "/momentum/static/js/main.dd02d4fa.chunk.js"
+  },
+  {
+    "revision": "12eb85f0bc0e39557d8d",
+    "url": "/momentum/static/js/runtime-main.c889f98a.js"
+  },
+  {
+    "revision": "cb45c976713ac648f459c769edeac9dd",
+    "url": "/momentum/static/media/bg.cb45c976.jpg"
+  }
+]);
